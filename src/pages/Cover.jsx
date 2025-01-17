dc1e1bee0e1b3f04b9340bfd3086e20b74972052
HansGit30 <homgit07@gmail.com>
2025-01-16 19:16:00 -0500

    arregladondo imagenes

--- src/pages/Cover.jsx
+++ src/pages/Cover.jsx
@@ -5,7 +5,7 @@ const Cover = () => {
     return (
         <div className="w-full h-screen overflow-hidden banner">
             <header className="w-full h-[10%] flex items-center px-4">
-                <img className="w-[100px]" src="/src/assets/LogoPokemon.png" alt="" />
+                <img className="w-[100px]" src="../assets/LogoPokemon.png" alt="" />
             </header>
             <section className="relative w-full h-[60%] flex justify-center items-end  p-10">
                 <div className="relative w-[60%] h-[70%] flex justify-center items-center">
@@ -21,7 +21,7 @@ const Cover = () => {
                 <h1 className="text-4xl mb-3">¡Atrápalos a todos!</h1>
                 <p className="px-4 mb-3">Explora, aprende y encuentra a tus Pokémon favoritos en un solo lugar.</p>
                 <button className="relative w-[200px] h-[50px] bg-white rounded-xl text-black">
-                    <img className="absolute top-2 left-2 w-9" src="/src/assets/Pokebola-pokeball-png-0-removebg-preview.png" alt="" />
+                    <img className="absolute top-2 left-2 w-9" src="../assets/Pokebola-pokeball-png-0-removebg-preview.png" alt="" />
                     GO!!
                 </button>
             </section>
