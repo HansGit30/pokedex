44f0bd18ae5ae22e0804b0af713192b0855b7bab
HansGit30 <homgit07@gmail.com>
2025-01-17 16:28:50 -0500

    c

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import "./Home.css"
 const Home = () => {
   return (
     <div>
+      {/* <nav className="fixed bottom-0 w-full h-[80px] bg-black">
+        <ul className="w-[90%] h-full bg-white mx-auto rounded-2xl"></ul>
+      </nav> */}
       <header className="w-full h-[100px] flex items-center justify-between px-4">
         <img className="w-[120px]" src={Logo} alt="" />
         <img className="w-10" src={Pokeball} alt="" />
@@ -12,10 +15,12 @@ const Home = () => {
       <article className="w-full h-[220px] flex items-center justify-center">
         <div className="w-[90%] h-[90%] rounded-2xl fondoBanner"></div>
       </article>
-      <aside className="w-full h-[70px] bg-slate-900 pl-4 flex items-center">
+      <aside className="w-full h-[70px] pl-4 flex items-center">
         <div className="w-[80px] h-10 rounded-3xl bg-slate-400 flex items-center justify-center">Todos</div>
       </aside>
       <section className="w-full grid grid-cols-2 gap-4 p-4">
+        <div className="h-[250px] bg-slate-500 rounded-2xl"></div>
+        <div className="h-[250px] bg-slate-500 rounded-2xl"></div>
         <div className="h-[250px] bg-slate-500 rounded-2xl"></div>
         <div className="h-[250px] bg-slate-500 rounded-2xl"></div>
       </section>
